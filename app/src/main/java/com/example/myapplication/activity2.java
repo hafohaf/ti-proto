3642382a1ca141f9fdeb00aff9d97b3704a2a10c
hafohaf <Chuan.Jin@ruhr-uni-bochum.de>
2023-06-09 21:00:02 +0200

    first commit

--- app/src/main/java/com/example/myapplication/activity2.java
+++ app/src/main/java/com/example/myapplication/activity2.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity2 extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+    }
+    public void goTomain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    public void goToubung(View view) {
+        Intent intent = new Intent(this, tableubung.class);
+        startActivity(intent);
     }
 }
